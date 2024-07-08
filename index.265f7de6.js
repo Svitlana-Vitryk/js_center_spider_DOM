20e1b64732f80a52df040398bd9302c8bf622821
Svitlana-Vitryk <vitriksvitlana@gmail.com>
2024-07-08 15:30:19 +0300

    make build

---
+++ index.265f7de6.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",function(){var e=document.querySelector(".wall"),t=document.querySelector(".spider"),n=e.clientWidth,c=e.clientHeight,i=t.clientWidth,l=t.clientHeight;t.style.left="".concat((n-i)/2,"px"),t.style.top="".concat((c-l)/2,"px")});
+//# sourceMappingURL=index.265f7de6.js.map
